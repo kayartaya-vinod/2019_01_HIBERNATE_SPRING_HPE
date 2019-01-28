f75b36e646d745bf6eb5cf2a7ca91e6a1a945d91
kayartaya.vinod <kayartaya.vinod@gmail.com>
2019-01-28 10:26:00 +0530

    🇮🇳

---
+++ Eclipse_workspace/training/src/main/java/com/hpe/training/programs/P07_GetEmployeeData.java
@@ -0,0 +1,36 @@
+package com.hpe.training.programs;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.hpe.training.entity.Employee;
+import com.hpe.training.utils.HibernateUtil;
+
+public class P07_GetEmployeeData {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = HibernateUtil.getSessionFactory();
+		
+		try {
+			Session session = factory.openSession();
+			Employee e1 = (Employee) session.get(Employee.class, 5);
+			
+			System.out.println("Name = " + e1.getFirstName() + " " + e1.getLastName());
+			System.out.println("City = " + e1.getContactDetails().getCity());
+			System.out.println("Manager = " + e1.getManager().getFirstName() 
+					+ " " + e1.getManager().getLastName());
+			
+			System.out.println();
+			System.out.println("Subordinates: ");
+			for(Employee sub: e1.getSubordinates()) {
+				System.out.println(sub.getFirstName() + " " + sub.getLastName());
+			}
+			
+
+			session.close();
+		} finally {
+			factory.close();
+		}
+	}
+}
