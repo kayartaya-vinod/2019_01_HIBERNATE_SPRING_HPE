febbd84732af0837eca274941fa9271b4474db25
kayartaya.vinod <kayartaya.vinod@gmail.com>
2019-01-28 09:13:27 +0530

    🇮🇳

---
+++ Eclipse_workspace/training/src/main/java/com/hpe/training/entity/Supplier.java
@@ -0,0 +1,99 @@
+package com.hpe.training.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "suppliers")
+public class Supplier {
+
+	@Id
+	@Column(name = "supplier_id")
+	private Integer supplierId;
+	@Column(name = "company_name")
+	private String companyName;
+	@Column(name = "contact_person")
+	private String contactPerson;
+	@Column(name = "contact_title")
+	private String contactTitle;
+
+	// composition (HAS-A relationship)
+	@Embedded
+	private ContactDetails contactDetails;
+
+	private String phone;
+	private String fax;
+	@Column(name = "home_page")
+	private String homePage;
+
+	public Supplier() {
+	}
+
+	public Integer getSupplierId() {
+		return supplierId;
+	}
+
+	public void setSupplierId(Integer supplierId) {
+		this.supplierId = supplierId;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getContactPerson() {
+		return contactPerson;
+	}
+
+	public void setContactPerson(String contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+
+	public String getContactTitle() {
+		return contactTitle;
+	}
+
+	public void setContactTitle(String contactTitle) {
+		this.contactTitle = contactTitle;
+	}
+
+	public ContactDetails getContactDetails() {
+		return contactDetails;
+	}
+
+	public void setContactDetails(ContactDetails contactDetails) {
+		this.contactDetails = contactDetails;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getHomePage() {
+		return homePage;
+	}
+
+	public void setHomePage(String homePage) {
+		this.homePage = homePage;
+	}
+
+}
--- Eclipse_workspace/training/src/main/java/com/hpe/training/programs/P01_GetCategoryById.java
+++ Eclipse_workspace/training/src/main/java/com/hpe/training/programs/P01_GetCategoryById.java
@@ -12,36 +12,32 @@ import com.hpe.training.entity.Product;
 public class P01_GetCategoryById {
 
 	public static void main(String[] args) {
-		
+
 		// empty configuration object
 		Configuration cfg = new Configuration();
-		
+
 		// load information from hibernate.cfg.xml
 		cfg.configure();
-		
 
-		ServiceRegistry registry = 
-				new StandardServiceRegistryBuilder()
-				.applySettings(cfg.getProperties())
-				.build();
-		
+		ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+
 		// build the session factory (represents a db session, singleton)
 		SessionFactory factory = cfg.buildSessionFactory(registry);
-		
+
 		// get a session (represents a live db connection)
 		Session session = factory.openSession();
-		
+
 		// get an instanceof Category from the db
 		// C --> save(), persist(), saveOrUpdate(), merge()
 		// R --> get(), load()
 		// U --> update(), merge(), saveOrUpdate()
 		// D --> delete()
-		
+
 		Integer id = 2;
 		// System.out.println("pass-1");
 		Category c = (Category) session.get(Category.class, id);
 		c = (Category) session.get(Category.class, id);
-		
+
 		// session.refresh(c);
 		//
 		// c = (Category) session.get(Category.class, id);
@@ -49,25 +45,17 @@ public class P01_GetCategoryById {
 		// System.out.println("pass-2");
 
 		// System.out.println("Got an instanceof " + c.getClass().getName());
-		
-		
+
 		System.out.println("Name        = " + c.getCategoryName());
 		System.out.println("Description = " + c.getDescription());
-		
+
 		System.out.println("Products in this category: ");
-		for(Product p: c.getProducts() ) {
+		for (Product p : c.getProducts()) {
 			System.out.println(p.getProductName() + ", $ " + p.getUnitPrice());
 		}
-		
+
 		session.close();
 		factory.close();
-			
-		
+
 	}
 }
-
-
-
-
-
-
