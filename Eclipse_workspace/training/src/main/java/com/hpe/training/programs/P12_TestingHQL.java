1f1fcb8c7aa024a88b0e82d15f0c83074324a73e
kayartaya.vinod <kayartaya.vinod@gmail.com>
2019-01-29 09:37:00 +0530

    🇮🇳

--- Eclipse_workspace/training/src/main/java/com/hpe/training/programs/P12_TestingHQL.java
+++ Eclipse_workspace/training/src/main/java/com/hpe/training/programs/P12_TestingHQL.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.Set;
 
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import com.hpe.training.entity.Category;
+import com.hpe.training.entity.Order;
 import com.hpe.training.entity.Product;
 import com.hpe.training.utils.HibernateUtil;
 
@@ -30,12 +32,49 @@ public class P12_TestingHQL {
 			printProductsByPage(4); // 4 --> pageNum (assuming 10 is the page size)
 			printProductNamesAndPrices(); // example for PROJECTION
 			printCategorywiseProductSummary(); // example of GROUP BY
-			incrementProductPriceBy(1.0); // inc price is $1
-			
+			// incrementProductPriceBy(1.0); // inc price is $1
+			printCustomersFromCity("London"); // city = "London"
+			printOrdersHavingOrderTotalMoreThan(10000.0); // orderTotal = 10000
+
 			session.close();
 		} finally {
 			factory.close();
 		}
+	}
+
+	static void printOrdersHavingOrderTotalMoreThan(double orderTotal) {
+		String sql = "select * from orders where order_id in " + 
+				"(select order_id from order_details " + 
+				"group by order_id " + 
+				"having sum(unit_price*quantity*(1-discount))>=:ORDER_TOTAL)";
+		SQLQuery qry = session.createSQLQuery(sql);
+		qry.setDouble("ORDER_TOTAL", orderTotal);
+		qry.addEntity(Order.class);
+		List<Order> list = qry.list();
+		for(Order ord: list) {
+			System.out.println("Order id: " + ord.getOrderId());
+			System.out.println("Customer: " + ord.getCustomer().getCompanyName());
+			System.out.println("Employee: " + ord.getEmployee().getFirstName());
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	static void printCustomersFromCity(String city) {
+		// Use SQL queries instead of HQL:
+		// 1. when the table under question is not mapped to any entity
+		// 2. when migrating your JDBC code into Hibernate code, the query may be too complex
+		// 3. when leveraging the DB features such as stored functions, procedures, builtin functions
+		
+		String sql = "SELECT * FROM CUSTOMERS where city = :CITY";
+		SQLQuery qry = session.createSQLQuery(sql);
+		qry.setString("CITY", city);
+		List<Object[]> list = qry.list();
+		System.out.println("Customers from " + city + " are: ");
+		for (Object[] data : list) {
+			System.out.printf("%s [%s]\n", data[1], data[2]);
+		}
+		System.out.println();
 	}
 
 	static void incrementProductPriceBy(double incPrice) {
@@ -51,25 +90,22 @@ public class P12_TestingHQL {
 			tx.rollback();
 			System.out.println("OOPS! Something went wrong! -- " + e.getMessage());
 		}
-		
+
 	}
 
 	static void printCategorywiseProductSummary() {
 		String hql = "select p.category.categoryName, count(p), avg(p.unitPrice), "
-				+ "sum(p.unitPrice * p.unitsInStock) from Product p "
-				+ "group by p.category.categoryName "
-				+ "having count(p) > 10 "
-				+ "order by p.category.categoryName ";
-				
+				+ "sum(p.unitPrice * p.unitsInStock) from Product p " + "group by p.category.categoryName "
+				+ "having count(p) > 10 " + "order by p.category.categoryName ";
+
 		Query qry = session.createQuery(hql);
 		List<Object[]> list = qry.list();
-		for(Object[] data: list) {
+		for (Object[] data : list) {
 			String catName = (String) data[0];
 			Long count = (Long) data[1];
 			Double avgPrice = (Double) data[2];
 			Double stockValue = (Double) data[3];
-			System.out.printf("%s (count=%d, avg=%.2f, stock_val=%.2f)\n",
-					catName, count, avgPrice, stockValue);
+			System.out.printf("%s (count=%d, avg=%.2f, stock_val=%.2f)\n", catName, count, avgPrice, stockValue);
 		}
 		System.out.println();
 	}
@@ -78,9 +114,9 @@ public class P12_TestingHQL {
 		String hql = "select productName, unitPrice from Product";
 		Query qry = session.createQuery(hql);
 		qry.setMaxResults(10); // limit the no.of records to fetch
-		
+
 		List<Object[]> list = qry.list();
-		for(Object[] data: list) {
+		for (Object[] data : list) {
 			String pname = (String) data[0];
 			Double price = (Double) data[1];
 			System.out.println(pname + " --> $" + price);
@@ -94,13 +130,10 @@ public class P12_TestingHQL {
 		Query qry = session.createQuery("from Product");
 		qry.setFirstResult(offset);
 		qry.setMaxResults(pageSize);
-		
+
 		List<Product> list = qry.list();
 		for (Product p : list) {
-			System.out.printf("%2d %-40s $%10.2f\n",
-					p.getProductId(),
-					p.getProductName(),
-					p.getUnitPrice());
+			System.out.printf("%2d %-40s $%10.2f\n", p.getProductId(), p.getProductName(), p.getUnitPrice());
 		}
 		System.out.println();
 	}
